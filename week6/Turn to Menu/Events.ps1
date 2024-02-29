a9a05d6d2f71466f0a6dbf99ed377d20d8f51ecd
samuel.richardson <samuel.richardson@mymail.champlain.edu>
2024-02-29 17:45:37 -0500

    Finished Week 6

---
+++ week6/Turn to Menu/Events.ps1
@@ -0,0 +1,61 @@
+﻿. (Join-Path $PSScriptRoot String-Helper.ps1)
+
+function ApacheLogs1() {
+$logsNotformated = Get-Content C:\xampp\apache\logs\access.log
+$tableRecords = @()
+
+for ($i=0; $i -lt $($logsNotformated.count); $i++) {
+$words = $logsNotformated[$i].Split(" ")
+
+
+$tablerecords += [pscustomobject]@{
+ "IP" = $words[0];
+ "Time" = $words[3].Trim('[');
+ "Method" = $words[5].Trim('"');
+ "Page" = $words[6];
+ "Protocol" = $words[7];
+ "Response" = $words[8];
+ "Referrer" = $words[10];
+ "Client" = $words[11..($words.count)];}
+}
+$tableRecords = ($tableRecords | Where-Object {$_.IP -ilike "10.*"})
+return $tableRecords[-10..-1]
+}
+
+function getFailedLogins(){
+  
+  $failedlogins = Get-EventLog security | Where { $_.InstanceID -eq "4625" }
+
+  $failedloginsTable = @()
+  for($i=0; $i -lt $failedlogins.Count; $i++){
+
+    $account=""
+    $domain="" 
+
+    $usrlines = getMatchingLines $failedlogins[$i].Message "*Account Name*"
+    $usr = $usrlines[1].Split(":")[1].trim()
+
+    $dmnlines = getMatchingLines $failedlogins[$i].Message "*Account Domain*"
+    $dmn = $dmnlines[1].Split(":")[1].trim()
+
+    $user = $dmn+"\"+$usr;
+
+    $failedloginsTable += [pscustomobject]@{"Time" = $failedlogins[$i].TimeGenerated; `
+                                       "Id" = $failedlogins[$i].InstanceId; `
+                                    "Event" = "Failed"; `
+                                     "User" = $user;
+                                     }
+
+    }
+
+    return ($failedloginsTable | sort Time)
+} # End of function getFailedLogins
+
+function startChrome(){
+    if ((Get-Process -Name "*chrome*") -ne $null){
+        Write-Host "Chrome already Running"
+    }
+    else{
+    Start-Process "C:\Program Files\Google\Chrome\Application\Chrome.exe" -ArgumentList '"https://champlain.edu"'
+    }
+}
---
+++ week6/Turn to Menu/main.ps1
@@ -0,0 +1,42 @@
+﻿. (Join-Path $PSScriptRoot Events.ps1)
+
+clear
+
+$Prompt  = "Please choose your operation:`n"
+$Prompt += "1 - Display last 10 apache logs`n"
+$Prompt += "2 - Display last 10 failed logins for all users`n"
+$Prompt += "3 - Display at risk users`n"
+$Prompt += "4 - Start Chrome`n"
+$Prompt += "5 - Exit`n"
+
+
+
+$operation = $true
+
+while($operation){
+
+    
+    Write-Host $Prompt | Out-String
+    $choice = Read-Host 
+
+    if ((-not ($choice -match "^[1-5]$"))){Write-Host "Not a valid input."; continue;}
+
+    if($choice -eq 5){
+        Write-Host "Goodbye" | Out-String
+        exit
+        $operation = $false 
+    }
+
+    if($choice -eq 1){$apacheLogs = ApacheLogs1; write-host ($apacheLogs | Format-Table | Out-String)}
+
+    elseif($choice -eq 2){$failedLogins = getFailedLogins; write-host ($failedLogins[-10..-1] | Format-Table | Out-String)}
+
+    elseif($choice -eq 3){
+
+        $failedLogins = getFailedLogins
+       
+        Write-Host ($failedLogins | Group-Object User | where-object {$_.count -ge 10} | select count, name | Format-Table | Out-String)
+    }
+    elseif($choice -eq 4){startChrome}
+
+}
